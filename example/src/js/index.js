a99dec67b5077b358b30368e285e31a383854a47
Simon <ssmdmsn@hotmail.com>
2017-05-04 20:50:02 +0800

    some example

---
+++ example/src/js/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <h1>hello work,a great job. hah
+    
+    a</h1>,
+    document.getElementById('example')
+);
